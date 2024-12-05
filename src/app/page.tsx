fbef6ad32a1185cb712f15d9cd1e3a61cad91da7
kaws26 <141999694+kaws26@users.noreply.github.com>
2024-12-05 14:05:33 +0000

    changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { HeroSection } from "@/components/heroSection";
-import { Boxes } from "@/components/ui/background-boxes";
-import skills from "@/components/skills";
-import { cn } from "@/lib/utils";
+
 import Skills from "@/components/skills";
 import ExperienceTimeline from "@/components/experience";
 
@@ -17,8 +15,7 @@ export default function Home() {
       </div>
       {/* Middle Section */}
       <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
-        {/* Overlay */}
-        <div className="absolute inset-0 w-full h-full bg-slate-900 z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        
         
         {/* Content */}
         
